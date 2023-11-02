3b51302f949a3f59596394c2b995b0f66a396cd7
Eduardo Tostado <eduardotostado@csu.fullerton.edu>
2023-11-01 23:21:23 -0700

    Finished Part 2

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,10 @@
-// TODO: Add the required header
+// Eduardo Tostado
+// eduardotostado@csu.fullerton.edu
+// @Eduardo-Tostado
+// Partners: @WuniorAyo
+//
+// Date Difference 2
+//
 
 #include <iostream>
 #include <string>
@@ -7,10 +13,14 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
+  // Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
-
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number"
+              << "\n";
+    return 1;
+  }
+  // Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
   // values.
@@ -18,14 +28,24 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
-
-  // TODO: After the loop has finished summing the arguments, calculate the
+  double sum{0.0};
+  int count{0};
+  bool first{true};
+  for (std::string const& argument : arguments) {
+    if (first) {
+      first = false;
+      continue;
+    }
+    sum += std::stod(argument);
+    count++;
+  }
+  // After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
+  double average = sum / count;
+  // Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
-
+  std::cout << "average = " << average;
   return 0;
 }
